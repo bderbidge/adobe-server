8cfdc4f9870cdc523bdea4b42607eec5d2538a5b
bderbidge <brander81@gmail.com>
2019-03-08 16:40:47 -0700

    Router is now an Asyn function.

--- routes/romanNumeralRoute.js
+++ routes/romanNumeralRoute.js
@@ -2,25 +2,25 @@ const romannumeral = require('../model/romanNumeral');
 var express = require('express');
 var router = express.Router();
 
-router.get('/romannumeral', (req, res, next) => {
+router.get('/romannumeral', async (req, res, next) => {
+  try {
+    let result = await romannumeral.execute(req);
 
-  romannumeral.execute(req)
-      .then((result) => {
-        if (result != null) {
-          res.status(200);
-          return res.send(result);
-        }
+    if (result != null) {
+      res.status(200);
+      return res.send(result);
+    }
 
-        res.status(400);
-        return res.end();
-      })
-      .catch((err) => {
-        res.status(500);
-        res.json({
-          error: err.message,
-        });
-        next();
-      });
+    res.status(400);
+    return res.end();
+
+  } catch (err) {
+    res.status(500);
+    res.json({
+      error: err.message,
+    });
+    next();
+  }
 });
 
 module.exports = router;
